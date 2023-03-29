6c707236cfde5ab57de619984294d8bbfde055bc
Foysal Ahmed Hridoy <96944139+foysal231@users.noreply.github.com>
2023-03-29 21:13:49 +0200

    Add files via upload

---
+++ Summing Consecutive Integers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main ()
+{
+    int a, n , i , t=0;
+    scanf("%d %d",&a, &n);
+    while (n<=0)
+
+        scanf("%d", &n);
+        for(i=1; i<=n;i++)
+        {
+            t+=a;
+            a++;
+        }
+
+    printf("%d\n",t);
+    return 0;
+
+}
